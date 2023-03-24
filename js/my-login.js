e37e9620e3c9d0df54b5b6b0fb0275095322668e
alpersuarsoft <67925972+alpersuarsoft@users.noreply.github.com>
2023-03-24 17:59:21 +0300

    Update my-login.js

--- js/my-login.js
+++ js/my-login.js
@@ -3,15 +3,12 @@
  *
  * Bootstrap 4 Login Page
  *
- * @author          Muhamad Nauval Azhar
- * @uri 			https://nauval.in
- * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
- * @license         My Login is licensed under the MIT license.
- * @github          https://github.com/nauvalazhar/my-login
- * @version         1.2.0
- *
- * Help me to keep this project alive
- * https://www.buymeacoffee.com/mhdnauvalazhar
+ * @author          Alper Süar
+ * @uri 			https://alpersuarsoft.github.io/
+ * @copyright       Copyright (c) 2023 Alper Süar
+ * @license         Lisans misans yok bam bam bam.
+ * @github          https://github.com/alpersuarsoft
+ * @version         1.0.0
  * 
  ******************************************/
 
